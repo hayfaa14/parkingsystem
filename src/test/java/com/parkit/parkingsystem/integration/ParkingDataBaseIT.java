e856b3c09f62f0e496bb37f418d0edd00d1043ef
hayfaa14 <79854730+hayfaa14@users.noreply.github.com>
2022-06-30 09:12:44 +0200

    Update ParkingDataBaseIT.java

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -7,7 +7,6 @@ import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
-
 import org.apache.commons.lang.time.DateUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -18,18 +17,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
 import java.text.DateFormat;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-
-
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -40,12 +35,9 @@ public class ParkingDataBaseIT {
     private static TicketDAO ticketDAO;
     private static DataBasePrepareService dataBasePrepareService;
    
-
-
     @Mock
     private static InputReaderUtil inputReaderUtil;
    
-
     @BeforeAll
     private static void setUp() throws Exception {
         parkingSpotDAO = new ParkingSpotDAO();
@@ -64,13 +56,11 @@ public class ParkingDataBaseIT {
     }
 
     @AfterAll
-    private static void tearDown(){
+    private static void tearDown() {
 
     }
-    
-    
     @Test
-    public void testParkingACar(){
+    public void testParkingACar() {
     	//WHEN
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
@@ -78,38 +68,24 @@ public class ParkingDataBaseIT {
         Ticket ticket = ticketDAO.getTicket("ABCDEF");
         String sut = ticket.getVehicleRegNumber();
         Boolean sut2 = ticket.getParkingSpot().isAvailable();
+        
         assertEquals("ABCDEF", sut);
         assertEquals(false, sut2);
     }
     
-    
-
     @Test
-    public void testParkingLotExit(){
+    public void testParkingLotExit() {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processExitingVehicle();
+        parkingService.processIncomingVehicle();
         Ticket ticket = ticketDAO.getTicket("ABCDEF");
-        
-        DateFormat dateFormatUsed = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
+        String sut = ticket.getVehicleRegNumber();
+        parkingService.processExitingVehicle();
+        Double sut = ticket.getPrice();
+        assertEquals(0 , sut );
         Date outTime = new Date();
-        Date outTimeNearestMin = DateUtils.round(outTime, Calendar.MINUTE);
-        String strDateEx = dateFormatUsed.format(outTimeNearestMin);
-        Date realTime = ticket.getOutTime();
-        Date realTimeNearestMin=DateUtils.round(realTime, Calendar.MINUTE);
-        String strDateReal = dateFormatUsed.format(realTimeNearestMin);
-        
-        Double realPrice=ticket.getPrice();
-        long inTime=ticket.getInTime().getTime();
-        long duration=outTime.getTime()-inTime;
-        Double exPrice = 1.5*(duration*(1/3.6)*1e-6);
-        double eps = 1e-03;
-      
-        assertEquals(strDateEx,strDateReal);
-        assertEquals(exPrice,realPrice,eps);
-         
-        //TODO: check that the fare generated and out time are populated correctly in the database
+        Timestamp expectedDate = new Timestamp(1000 *((outTime.getTime())/1000));
+        Date sut2 = ticket.getOutTime();
+        assertEquals(expectedDate, sut2);
     }
-
-
 
 }
